fcf2e44e6c4017289d6c9d6d81c5512d5b365c47
DominicDev <s0l0estudi4r@gmail.com>
2025-11-02 14:26:50 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' KALEO ';
+export let nameSongs = [
+	"WAY DOWN WE GO", /*1*/
+	"NO GOOD", /*2*/
+	"WAY DOWN WE GO - TIKTOK VERSION", /*3*/
+	"ALL THE PRETTY GIRLS", /*4*/
+	"WAY DOWN WE GO - EDIT", /*5*/
+	"I CAN'T GO ON WITHOUT YOU", /*6*/
+	"BROKEN BONES", /*7*/
+	"AUTOMOBILE", /*8*/
+	"VOR I VAGLASKÓGI", /*9*/
+	"GLASS HOUSE", /*10*/
+	"HOT BLOODED", /*11*/
+	"SAVE YOURSELF", /*12*/
+	"ROCK 'N' ROLLER", /*13*/
+	"I WANT MORE", /*14*/
+	"BACKBONE", /*15*/
+	"SKINNY", /*16*/
+	"I WALK ON WATER", /*17*/
+	"ALTER EGO", /*18*/
+	"HEY GRINGO", /*19*/
+	"ELECTRICITY", /*20*/
+]
